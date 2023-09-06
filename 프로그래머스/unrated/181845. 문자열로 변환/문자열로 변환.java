7a0cad0233c41d158194647aa4ef64e5529125b5
imjae <121646578+imjaehy0b@users.noreply.github.com>
2023-09-06 22:28:36 +0900

    Update 문자열로 변환.java

--- 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -2,7 +2,8 @@ class Solution {
     public String solution(int n) {
         String answer = String.valueOf(n);
         
-        
+        //처음에는
+        //String answer = "" + n; 형식으로 했음 -> 처리시간이 너무 늘어남 -> 메서드 사용
         
         return answer;
     }
