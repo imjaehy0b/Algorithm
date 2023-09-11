ea0ef7e979503f77662b211857ba108cfa3be73b
imjae <121646578+imjaehy0b@users.noreply.github.com>
2023-09-11 16:53:33 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,8 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[10];
-        
-        answer = my_string.split(" ");
+        String[] answer = my_string.split(" ");
         return answer;
     }
 }
