25979583da0d69fe7b62b2751b6e32d5c6aa1279
imjae <imjae950@gmail.com>
2023-09-12 16:51:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 6.55 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,14 +3,9 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String test = ""+n;
-        for (int i = 0; i < test.length(); i++) {
-           answer += test.charAt(i)-48;
-        }
-        
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println(answer);
+        System.out.println("Hello Java");
 
         return answer;
     }
