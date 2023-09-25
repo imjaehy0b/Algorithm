131556a4c2b897aafc26df45de0866dc99811907
imjae <imjae950@gmail.com>
2023-09-25 15:47:15 +0900

    Update 최댓값 만들기 （2）.java

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,6 +2,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] num) {
         int answer = 0;
+        // int max = 0;
+        //이 부분에서 -n * n 의 경우를 생각하지 못했음.
+        //질문에서 힌트를보고 초기화 값을 음수로  시작함.
         int max = Integer.MAX_VALUE * -1;
         
         for(int i=0;i<num.length;i++){
