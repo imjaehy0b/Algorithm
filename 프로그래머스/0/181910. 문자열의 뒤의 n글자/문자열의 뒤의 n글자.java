e164ece415ee597075111d76ce840a1d4cb0a90c
imjae <imjae950@gmail.com>
2024-04-12 23:17:58 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 1.62 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
         int start = my_string.length()-n;
-        answer += my_string.substring(start,n+start);
+        answer += my_string.substring(start);
         return answer;
     }
 }
