182c028de222dd2b7060e1e0b4118fa2b6168e31
imjae <imjae950@gmail.com>
2024-04-16 16:45:55 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 2.49 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String str = "";
+        for(int i=0; i<myString.length();i++){
+            if(myString.charAt(i)=='A'){
+                str+= "B";
+            }else{
+                str+= "A";
+            }
+        }
+        
+        return answer = str.contains(pat) ? 1 : 0;
+    }
+}
