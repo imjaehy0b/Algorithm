0476ee15ec95326045e98de95c8688013b853776
imjae <imjae950@gmail.com>
2024-04-22 23:20:50 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String[] str = new String[array.length];
+        for(int i=0; i<str.length; i++){
+            str[i] = String.valueOf(array[i]);
+        }
+        
+        for(int i=0; i<str.length; i++){
+            for(int j=0; j<str[i].length(); j++){
+                if(str[i].charAt(j)=='7'){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
