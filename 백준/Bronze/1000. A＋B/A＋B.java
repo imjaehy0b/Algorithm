022794927f69adcaf455233b0c745c37bb5c50d9
imjae <imjae950@gmail.com>
2024-04-27 22:17:47 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,9 @@
 import java.util.*;
 public class Main{
-    public static void main(String[] args){
-        //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        System.out.print(A+B);
-    } 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
 }
