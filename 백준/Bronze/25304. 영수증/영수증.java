24a9fe23ab3ddc69874f05161a0debbe21a1fab4
imjae <imjae950@gmail.com>
2024-04-28 19:43:14 +0900

    [Bronze IV] Title: 영수증, Time: 224 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        int total = 0;
+        for(int i=0; i<N; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            total += a*b;
+        }
+        if(total==X){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+        
+    }
+}
