f7d9abd49f3a9c7a892f73a8946d24ad6c87844b
imjae <imjae950@gmail.com>
2024-05-01 16:08:23 +0900

    [level 0] Title: 중복된 문자 제거, Time: 12.05 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -3,7 +3,6 @@ class Solution {
         String answer = "";
         //0,1,2,3,4...
         //i의 자리 인덱스값과 indexOf의 자리 인덱스값이 일치한다면 중복값이 없는것
-        //why?-> indexOf는 마지막인덱스를 출력하기때문.
         for(int i=0; i<my_string.length(); i++){
             if(i==my_string.indexOf(String.valueOf(my_string.charAt(i)))){
                 answer += my_string.charAt(i);
