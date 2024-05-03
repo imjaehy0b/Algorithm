7f514ec5fed6c3f3c1a606d34e96771d368b96d7
imjae <imjae950@gmail.com>
2024-05-04 00:07:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.13 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = false;
+        String[] str = String.valueOf(x).split("");
         int sum = 0;
-        int temp = x;
-        //x의 자릿수의 합으로 x가 나눠져야함
-        //18 => 1+8=9, 18%9==0, : 하샤드 
-        while(x>0){
-            sum+=x%10;
-            x/=10;
+        for(String s : str){
+            sum += Integer.parseInt(s);
         }
-        if(temp%sum==0) answer = true;
-        
+        if(x%sum==0){
+            answer = true;
+        }
         return answer;
     }
 }
