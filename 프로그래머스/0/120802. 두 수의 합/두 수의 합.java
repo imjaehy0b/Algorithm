f5a7b0b6e25f55820934149e831aa500b7f27de8
jaehyeop im <imjae950@gmail.com>
2024-10-11 09:46:46 +0900

    [level 0] Title: 두 수의 합, Time: 0.95 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -1,11 +1,7 @@
+import java.util.stream.*;  // Add this import
+
 class Solution {
     public int solution(int num1, int num2) {
-        boolean val = (-50000<=num1&&num1<=50000 && -50000<=num2&&num2<=50000);
-        int answer = -1;
-        
-        if(val) return num1 + num2;
-        
-        
-        return answer;
+        return IntStream.of(num1,num2).sum();
     }
 }
