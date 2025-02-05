2c6ad01647185a6c8a829e70bde4fa9a944ac55c
jaehyeop im <imjae950@gmail.com>
2025-02-05 15:23:30 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+    
+    public void solution() throws Exception{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write('1');
+        bw.write('\n'); 
+        bw.write('0');
+        bw.flush();
+        bw.close();
+        
+    }
+}
