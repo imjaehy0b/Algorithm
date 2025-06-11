5480dd2b9625ce39e1e64649e77e95298e2272e7
jaehyeop im <imjae950@gmail.com>
2025-06-12 08:54:33 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args){
+ 
+		StringBuilder sb = new StringBuilder();
+		sb.append("강한친구 대한육군\n");
+		sb.append("강한친구 대한육군");
+		
+		System.out.println(sb);
+	}
+}
