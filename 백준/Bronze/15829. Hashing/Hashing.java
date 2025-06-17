0a7473bd6c77f97ef18cad01e44f2e60ae2c607b
jaehyeop im <imjae950@gmail.com>
2025-06-17 11:17:25 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 14376 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,22 +9,25 @@ public class Main{
     
     private void solution() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         
         int cnt = Integer.parseInt(br.readLine());
         String str = br.readLine();
         
         BigInteger result = BigInteger.ZERO;
+        BigInteger power = BigInteger.ONE;
         BigInteger base = BigInteger.valueOf(31);
+        BigInteger mod = BigInteger.valueOf(1234567891);
 
         for(int i = 0; i < str.length(); i++){
             char ch = str.charAt(i);
             int value = ch - 'a' + 1;
-            BigInteger term = BigInteger.valueOf(value).multiply(base.pow(i));
-            result = result.add(term);
+            
+            BigInteger temp = BigInteger.valueOf(value).multiply(power);
+            result = result.add(temp).remainder(mod);
+            
+            power = power.multiply(base).remainder(mod);
             
         }
-        sb.append(result);
-        System.out.print(sb);
+        System.out.print(result);
     }
 }
