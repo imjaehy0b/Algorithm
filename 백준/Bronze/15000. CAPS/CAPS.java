ace5f574d804cbabe7cefa85fb80d8cc483216e6
jaehyeop im <imjae950@gmail.com>
2025-06-23 11:19:55 +0900

    [Bronze V] Title: CAPS, Time: 280 ms, Memory: 21188 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+    
+    private void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().toUpperCase());
+    }
+}
